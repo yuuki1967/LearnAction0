d3f7d259151f9f592a520172b1684af9499e6856
yuuki1967 <yuki06.endo@gmail.com>
2022-05-23 14:05:52 +0900

    modifiy yml and client.js

--- client.js
+++ client.js
@@ -1,22 +1,29 @@
-import sql from './db.js'
-async function getUsersOver(age) {
-	const users = await sql `
-		select
-			name,
-			age
-		from users
-		where age > ${ age }
-	`
-	return users
-}
+const { Client } = require('pg');
 
-async function insertUser( { name, age } ){
-	const users = await sql `
-		insert users
-			(name, age)
-		values 
-			(${ name }, ${ age })
-		returning name, age
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+});
 
-		return users
-}
+pgclient.connect();
+
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
+const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
+const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+
+pgclient.query(table, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query(text, values, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query('SELECT * FROM student', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+    pgclient.end()
+});
